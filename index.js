5ee2a63b3f87d64bfc8995d36e207551a658ea28
Brandon Pinto <b.pintony@gmail.com>
2022-07-20 11:06:32 -0400

    commit tree 57e5a1e21962ce17406e81ebc059bf2db41723d7

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-// Code your solution in this file!
+// const returnFirstTwoDrivers = function(S) {
+//     return S.slice(0,2);
+// };
+// const returnLastTwoDrivers = function(T) {
+//     return T.slice(-2);
+// };
+// const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// const createFareMultiplier = function (multiplyValue) {
+//     return function (value) {
+//          console.log('hello')
+//          return multiplyValue * value;
+//     };
+// };
+// const fareDoubler = createFareMultiplier(2);
+//let result = fareDoubler(3) 
+// const fareTripler = createFareMultiplier(3);
+
+// const selectDifferentDrivers = function (drivers, driversToReturn) {
+//   return driversToReturn(drivers);
+// };
+
+const returnFirstTwoDrivers = function(wheel) {
+    return wheel.slice(0,2);
+}
+const returnLastTwoDrivers = function(B) {
+    return B.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+        return multiplyValue*value
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+// let result = fareDoubler(3);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, driversToReturn) {
+  return driversToReturn(drivers);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
